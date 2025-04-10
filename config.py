a1ef0021b6b8689f946bf2391703eda1b5f68d12
Pravinsk28 <kamaraj2528@gmail.com>
2025-04-10 12:31:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os, re
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7819649400:AAHrXcfn6Wb6gLAU12ASyqf0q6TktsSFH7M")
+API_ID = int(os.environ.get("API_ID", "28987210"))
+API_HASH = os.environ.get("API_HASH", "04f69b63570297dac4e35948e4c6b4b7")
+OWNER = int(os.environ.get("OWNER", "5239080571"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "MB_rename_bot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "HxBots")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002031041556"))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "moviesblaster_linkz")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002474879896"))
 
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://Pravin:944244@cluster0.qe3bn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "Cluster0")
 
 STRING = os.environ.get("STRING", "")
 BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/cca849a2f63053fa3f622.jpg")
@@ -22,6 +22,6 @@ BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/cca849a2f63053fa3f62
 
 # if you need to add verify system then dm me on telegram
 
-SHORTNER_URL = os.environ.get("SHORTNER_URL", "")
-SHORTNER_API = os.environ.get("SHORTNER_API", "")
-TOKEN_TIMEOUT = os.environ.get("TOKEN_TIMEOUT", "")
+SHORTNER_URL = os.environ.get("SHORTNER_URL", "Modijiurl.com")
+SHORTNER_API = os.environ.get("SHORTNER_API", "5846ff3a30d0634b456cb04867154032a7574eac")
+TOKEN_TIMEOUT = os.environ.get("TOKEN_TIMEOUT", "14400")
